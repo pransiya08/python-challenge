8dca86e36ec0ea5ad1c43166856c548d741d6168
pransiya08 <pransiyacolumbino@gmail.com>
2023-03-15 19:54:50 +1100

    assignment final version

--- PyBank/main.py
+++ PyBank/main.py
@@ -4,62 +4,72 @@ import csv
 #accessing the budget_data.csv file in the Resources folder
 budget_csvpath = os.path.join("Resources","budget_data.csv")
 
-#COMMENT
-analysis_txtpath = os.path.join("analysis","analysis_result.txt")
+#creating the txt file and path for the result of the analysis
+analysis_txtpath = os.path.join("analysis","budgetanalysis_result.txt")
 
-#defining the analyse_budget() function
+#creating the analyse_budget() function
 def analyse_budget(month_data, budget_data):
 
     #printing the output in a .txt file
     with open(analysis_txtpath, "w") as f:
+        print("",file=f)
         print("Financial Analysis", file=f)
+        print("",file=f)
         print("------------------------------------------------------", file=f)
+        print("",file=f)
     
         #the total number of months included in the dataset can be identified by getting the length of the budget data (1 month: 1 p/l)
         total_month = len(month_data)
+        #print(total_month) #check the total month result
         print("Total Months: " + str(total_month), file=f)
+        print("",file=f)
 
-        #the net total amount of "Profit/Losses" over the entire period
+        #calculating the net total amount of "Profit/Losses" over the entire period
         nettotal_PL = 0
+        #creating a for loop to sum up the monthly budget over the entire period and printing the final/total budget
         for budget in budget_data:
             nettotal_PL += int(budget)
-        print("Total: $"+"{:,.2f}".format(nettotal_PL), file=f)  
+        #print(netotal_PL) #check the final budget total result
+        print("Total: $"+"{:,.2f}".format(nettotal_PL), file=f) 
+        print("",file=f) 
 
-        #the changes in "Profit/Losses" over the entire period, and then the average of those changes
+        #caluclating the changes in "Profit/Losses" over the entire period, and then the average of those changes
         change_PL = []
-
-        #COMMENT
+        #creating a for loop to store the changes between the current and previous month in an array
         for x in range(1,total_month):
             diff = int(budget_data[x]) - int(budget_data[x-1])
             change_PL.append(diff)
-        #print(change_PL)
+        #print(change_PL) #check the change array results
 
-        #COMMENT
+        #calculating the average change for the entire period 
         changetotal_PL = 0
+        #creating a for loop to sum up the total changes over the entire period then getting the average
         for change in change_PL:
             changetotal_PL += int(change)
         changeave_PL = changetotal_PL/(total_month-1)
-        print("Average Change: $"+"{:,.2f}".format(changeave_PL), file=f) 
+        #print(changeave_PL) #check the average change result
+        print("Average Change: $"+"{:,.2f}".format(changeave_PL), file=f)
+        print("",file=f) 
 
-        #the greatest increase in profits (date and amount) over the entire period
+        #identify the greatest increase in profits (date and amount) over the entire period
         max_PL = max(change_PL)
-        #COMMENT       
-        for budget in range(0,total_month-1):
+        #creating a for loop to check all rows and match with the identified max change and print the corresponding month     
+        for budget in range(total_month-1):
             if max_PL == change_PL[int(budget)]:
                 max_month = str(month_data[int(budget+1)])
+        #print(max_month , max_PL) check the max month and change results         
         print("Greatest Increase in Profits: " + (max_month) + " ($"+"{:,.2f}".format(max_PL)+")", file=f)
+        print("",file=f)
 
 
-        #the greatest decrease in profits (date and amount) over the entire period
+        #identify the greatest decrease in profits (date and amount) over the entire period
         min_PL = min(change_PL)
-        #COMMENT    
+        #creating a for loop to check all rows and match with the identified min change and print the corresponding month  
         for budget in range(0,total_month-1):
             if min_PL == change_PL[int(budget)]:
                 min_month = str(month_data[int(budget+1)])
+        #print(min_month , min_PL) check the min month and change results
         print("Greatest Decrease in Profits: " + (min_month) + " ($"+"{:,.2f}".format(min_PL)+")", file=f)
-
-        #COMMENT
-        f.close()
 
         return 
 
@@ -75,11 +85,12 @@ with open(budget_csvpath) as csvfile:
     budget_csvheader = next(budget_csvreader)
     #print(f"Budget Data Header: {budget_csvheader}")
 
-    #COMMENT
+    #getting the necessary data from the csv file
     budget_data = []
     month_data = []
     for row in budget_csvreader:
         month_data.append(row[0])
         budget_data.append(row[1])
 
+#running the function analyse_budget
 analyse_budget(month_data, budget_data)
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -4,75 +4,68 @@ import csv
 #accessing the election_data.csv file in the Resources folder
 election_csvpath = os.path.join("Resources","election_data.csv")
 
-#COMMENT
-poll_txtpath = os.path.join("analysis","poll_result.txt")
+#creating the txt file and path for the result of the poll
+poll_txtpath = os.path.join("analysis","pollanalysis_result.txt")
 
+#creating the election_result() function
 def election_result(candidate_data):
 
     #printing the output in a .txt file
     with open(poll_txtpath, "w") as f:
-        #print("Election Results")
         print("Election Results", file=f)
         print("", file=f)
         print("------------------------------------------------------", file=f)
         print("", file=f)
 
-        #the total number of votes cast
+        #calculate the total number of votes cast
         total_votes = len(candidate_data)
-        #print("Total Votes: " + str(total_votes))
+        #print(total_votes) #check the total vote result
         print("Total Votes: " + str(total_votes), file=f)
         print("", file=f)
         print("------------------------------------------------------", file=f)
         print("", file=f)
 
-        #the list of candidates who received votes, with their vote %, and total number of votes received
+        #get the list of unique candidates included in the election/poll
         candidates = []
         get_unique = [candidates.append(candidate) for candidate in candidate_data if candidate not in candidates]
         #print(candidates)
 
-        #COMMENT
+        #setting up the array for the vote counts
         counts = [0]*len(candidates)
 
-        #
+        #creating a for loop to add a vote count to a candidate by matching up with them in the candidates array
         for candidate in range(len(candidate_data)):
             if candidate_data[candidate] == candidates[0]:
                 counts[0] += 1
-                #print(candidates[0])
             elif candidate_data[candidate] == candidates[1]:
                 counts[1] += 1
-                #print(candidates[1])
             else:
                 counts[2] += 1
-                #print(candidates[2])
 
+        #calculating the percentage of the total votes received by a candidate
         percentages = [count/total_votes for count in counts]
 
-        #PRINT COMMENT
+        #creating a for loop to print out the candidates who received votes, with their vote %, and total number of votes received from the calculated arrays above
         for i in range(len(candidates)):
-            #print(str(candidates[i]) + ": " + "{:.3%}".format(percentages[i]) + " (" + str(counts[i]) +")")
             print(str(candidates[i]) + ": " + "{:.3%}".format(percentages[i]) + " (" + str(counts[i]) +")", file=f)
         print("", file=f)
         print("------------------------------------------------------", file=f)
         print("", file=f)
 
-        #COMMENT
-        results = dict(zip(candidates,percentages))
-        #print(results)
-
-        # WINNER COMMENT
+        #identifying the winner by getting the max vote% (vote count can be used too)
         winner = max(percentages)
+        #creating a for loop to identify the index of the max vote% in the percentages array to identify which candidate it corresponds to
         index = 0
         for i in range(len(percentages)):
             if percentages[i] == winner:
                 index = i
         
-        #print("Winner: " + str(candidates[index])) 
+        #print(candidates[index]) #to check the winner 
         print("Winner: " + str(candidates[index]),file=f)  
         print("", file=f)
         print("------------------------------------------------------", file=f)
         print("", file=f)
 
-        f.close()
         return
 
 
@@ -87,10 +80,11 @@ with open(election_csvpath) as csvfile:
     election_csvheader = next(election_csvreader)
     #print(f"Election Data Header: {election_csvheader}")
 
-    #COMMENT
+    #getting the necessary data from the csv file
     candidate_data = []
     for row in election_csvreader:
         candidate_data.append(row[2])
     #print(candidate_data)
 
+#running the function election_result
 election_result(candidate_data)
